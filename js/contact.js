d1c122e64edc498b5772bd226fc67f4beb2c0ef8
Tom Melosso <melossotom57@gmail.com>
2025-03-05 15:08:45 +0100

    ⚡️ Contact working + adding project + CV modification

--- js/contact.js
+++ js/contact.js
@@ -1,19 +1,21 @@
+document.addEventListener("DOMContentLoaded", function () {
+    emailjs.init("Nb-se0QXkVLz1JsfW");
 
-(function() {
-    emailjs.init("JUk9-axTr4XRXqNIS");
-})();
+    document.getElementById("contact-form").addEventListener("submit", function (event) {
+        event.preventDefault();
+        let params = {
+            name: document.getElementById("name").value,
+            email: document.getElementById("email").value,
+            subject: document.getElementById("subject").value,
+            message: document.getElementById("message").value
+        };
 
-
-document.getElementById("contact-form").addEventListener("submit", function(event) {
-    event.preventDefault(); 
-
-    var formData = new FormData(this); 
-
-    emailjs.sendForm("service_17tzkfh", "template_rixycb8", formData)
-        .then(function(response) {
-        alert("Message envoyé avec succès!");
-        }, function(error) {
-        alert("Erreur lors de l'envoi du message.");
-        });
+        emailjs.send("service_17tzkfh", "template_rixycb8", params)
+            .then(function (response) {
+                alert("Message envoyé avec succès !");
+                document.getElementById("contact-form").reset();
+            }, function (error) {
+                alert("Erreur lors de l'envoi du message : " + error.text);
+            });
+    });
 });
-
